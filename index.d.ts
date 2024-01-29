97108f2c7c0638a55788efe43142d9a83cd21a2d
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-28 19:12:21 -0500

    add type declarations, update package.json

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import { Plugin } from 'unified';
+import { Root } from 'mdast';
+
+// The plugin does not take options, so the first generic parameter is set to void[]
+// The plugin operates on a Root node from the mdast, aligning with the structure of a Markdown AST
+interface RemarkSectionizePlugin extends Plugin<void[], Root> {
+  // The plugin function takes a Root node as its parameter and modifies it in place, hence returning void
+  (tree: Root): void;
+}
+
+declare const remarkSectionize: RemarkSectionizePlugin;
+
+export = remarkSectionize;
