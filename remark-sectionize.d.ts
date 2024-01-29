6844502a9be8715d86d29177f351578dc25c35ca
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-01-28 20:25:36 -0500

    Remove unused declarations

--- remark-sectionize.d.ts
+++ remark-sectionize.d.ts
@@ -1,30 +1,15 @@
 // remark-sectionize.d.ts
 
-declare module 'remark-sectionize' {
+declare module "remark-sectionize" {
+  import { Node } from "unist";
 
-    import { Node } from 'unist';
-  
-    interface SectionNode extends Node {
-      type: 'section';
-      depth: number;
-      children: Node[];
-      data?: {
-        hName: string; 
-      }
-    }
-  
-    interface Plugin {
-      (): Transformer;
-    }
-  
-    type Transformer = (tree: Node) => void;
-  
-    interface Config {
-      maxDepth?: number;
-    }
-  
-    const remarkSectionize: Plugin;
-  
-    export default remarkSectionize;
-  
+  interface Plugin {
+    (): Transformer;
   }
+
+  type Transformer = (tree: Node) => void;
+
+  const remarkSectionize: Plugin;
+
+  export default remarkSectionize;
+}
